fa7c6f229cec907afc14e05595ad6b8e7329edf7
Eralp Çelebi <eralp_celebi@hotmail.com>
2019-10-20 19:39:20 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// I have to do this cuz I spent too much time on buttons
+
+function twitter() {
+  alert("Yoo")
+  window.location.replace("https://twitter.com/EralpCelebi");
+}
+
+function github() {
+  window.location.replace("https://gihub.com/EralpCelebi");
+}
